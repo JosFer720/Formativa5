52ec4ed2d5bf610f691a1090e4ded97490d19ce2
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-10-06 18:03:02 -0600

    Add files via upload

---
+++ criba.py
@@ -0,0 +1,43 @@
+#ACTIVIDAD FORMATIVA 5
+#MATEMATICA DISCRETA 1 - 10
+#FERNANDO RUIZ - 23065
+#--
+#--
+
+import time
+
+def criba(n):
+    not_prime = set()
+    primes = []
+    for i in range(2, n):
+        if i in not_prime:
+            continue
+
+        for j in range(i*2, n, i):
+            not_prime.add(j)
+        primes.append(i)
+    return primes
+
+def main(n):
+    prime_list = criba(int(n**0.5))
+
+    for i in prime_list:
+        if n % i == 0:
+            return False
+    return True
+
+while True:
+    try:
+        a = int(input("Ingrese un numero entero positivo: "))
+        if a > 0:
+            break
+        else:
+            print("No es un numero entero positivo.")
+    except ValueError:
+        print("Solo numeros,")
+
+start_time = time.time()
+print("¿Es primo?", main(a))
+print("Criba de Eratostenes hasta", a, ":", criba(a))
+end_time = time.time()
+print("Se ejecutó en " + str(end_time - start_time) + " segundos.")
---
+++ isprime.py
@@ -0,0 +1,55 @@
+#ACTIVIDAD FORMATIVA 5
+#MATEMATICA DISCRETA 1 - 10
+#FERNANDO RUIZ - 23065
+#--
+#--
+
+import time
+
+def prime_test1(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def criba(n):
+    not_prime = set()
+    primes = []
+    for i in range(2, n):
+        if i in not_prime:
+            continue
+
+        for j in range(i * 2, n, i):
+            not_prime.add(j)
+        primes.append(i)
+    return primes
+
+def first_div(n):
+    prime_list = criba(int(n ** 0.5) + 1)
+
+    for i in prime_list:
+        if n % i == 0:
+            return i  
+    return True
+
+# MAIN
+while True:
+    try:
+        a = int(input("Ingrese un número entero positivo: "))
+        if a > 0:
+            break
+        else:
+            print("No es un numero entero positivo.")
+    except ValueError:
+        print("Solo numeros.")
+
+start_time = time.time()
+
+result = first_div(a)
+if result is True:
+    print(f"El numero {a} es primo.")
+else:
+    print(f"El numero {a} no es primo, porque lo divide {result}.")
+
+end_time = time.time()
+print("Se ejecuto en " + str(end_time - start_time) + " segundos.")
